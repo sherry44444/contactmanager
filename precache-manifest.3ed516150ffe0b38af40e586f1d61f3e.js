3d41946c3467006eba83ae49ee192f959d32acb8
Hoang Xuan Lam <toi.di.tjm.toj@gmail.com>
2019-06-14 14:29:10 +0700

    Updates

---
+++ precache-manifest.3ed516150ffe0b38af40e586f1d61f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264d9b8ffadfe0a863f292c6ccfb59f9",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b3851239b5b714be4328",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a796c83f009bbe264194",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b3851239b5b714be4328",
+    "url": "/contactmanager/static/js/2.15852273.chunk.js"
+  },
+  {
+    "revision": "a796c83f009bbe264194",
+    "url": "/contactmanager/static/js/main.4b663610.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
